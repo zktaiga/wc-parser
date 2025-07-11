0140aba9200f499588bde5892fe2e8263634c490
Taiga <taiga@null.net>
2025-07-11 15:25:35 +0400

    bump

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -11,24 +11,24 @@ Is everything alright?"#;
 
 #[test]
 fn test_parse_string_empty() {
-    assert_eq!(parse_string("", None).len(), 0);
+        assert_eq!(parse_string("", None).unwrap().len(), 0);
 }
 
 #[test]
 fn test_parse_string_count() {
-    let messages = parse_string(CHAT_EXAMPLE, None);
+    let messages = parse_string(CHAT_EXAMPLE, None).unwrap();
     assert_eq!(messages.len(), 5);
 }
 
 #[test]
 fn test_parse_string_multiline() {
-    let messages = parse_string(CHAT_EXAMPLE, None);
+    let messages = parse_string(CHAT_EXAMPLE, None).unwrap();
     assert_eq!(messages[4].message, "How are you?\nIs everything alright?");
 }
 
 #[test]
 fn test_issue_237() {
-    let messages = parse_string("30/12/2020 13:00 - a: m\n13/1/2021 13:00 - a: m", None);
+    let messages = parse_string("30/12/2020 13:00 - a: m\n13/1/2021 13:00 - a: m", None).unwrap();
     assert_eq!(
         messages[0].date,
         Utc.with_ymd_and_hms(2020, 12, 30, 13, 0, 0).unwrap()
